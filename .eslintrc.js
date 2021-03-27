0e6f9d50862bda5e3c1c11519e1cb4552c42efb9
Jorge Barnaby <jorge.barnaby@gmail.com>
2021-03-27 00:05:41 -0400

    Update eslint-config-prettier to version 8.1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     extends: [
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'prettier',
         'plugin:prettier/recommended'
     ],
     parser: '@typescript-eslint/parser',
